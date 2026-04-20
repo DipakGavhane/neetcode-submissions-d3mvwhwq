689679635b794f5223b101a2342cb60cb0d73f91
DipakGavhane <151832554+DipakGavhane@users.noreply.github.com>
2026-04-20 10:04:46 +0530

    Add: longest-common-prefix - submission-6

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        # Step 1: Sort list
+        strs.sort()
+
+        # Step 2: Compare first and last items 
+        first = strs[0]
+        last = strs[-1]
+        prefix = ""
+
+        for i in range(min(len(first), len(last))):
+            if first[i] != last[i]:
+                break
+            prefix += first[i]
+            
+        
+        # Step 3: Return common chars
+        return prefix

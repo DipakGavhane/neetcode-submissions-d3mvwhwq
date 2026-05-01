3e6662ecb7a351f0711cfa723e464a963e45e1ce
DipakGavhane <151832554+DipakGavhane@users.noreply.github.com>
2026-05-01 17:56:43 +0530

    Add: design-hashmap - submission-1

---
+++ Data Structures & Algorithms/design-hashmap/submission-1.py
@@ -0,0 +1,52 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.size = 1000
+        self.buckets = [[] for _ in range(self.size)]
+
+        
+    def _hash(self, key):
+        index = key % self.size
+        return index
+
+
+    def put(self, key: int, value: int) -> None:
+        index = self._hash(key)
+        bucket = self.buckets[index]
+
+        # Update the value
+        for pair in bucket:
+            if pair[0] == key:
+                pair[1] = value
+                return
+
+        # Add now value, if not exist
+        bucket.append([key, value])
+        
+
+    def get(self, key: int) -> int:
+        index = self._hash(key)
+        bucket = self.buckets[index]
+
+        for pair in bucket:
+            if pair[0] == key:
+                return pair[1]
+
+        return -1
+
+
+    def remove(self, key: int) -> None:
+        index = self._hash(key)
+        bucket = self.buckets[index]
+
+        for i, pair in enumerate(bucket):
+            if pair[0] == key:
+                bucket.pop(i)
+                return
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
